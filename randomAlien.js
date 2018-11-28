5438f174f971cef8ae7dfffd9a9b18df0f392746
Michele <m.diegoli@scsitaly.com>
2018-11-28 08:34:39 +0100

    Update randomAlien.js

--- randomAlien.js
+++ randomAlien.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js"></script>
-</head>
-<body>
 
-    <script>
     var config = {
         type: Phaser.AUTO,
         width: 800,
@@ -53,7 +46,3 @@
 
         emitter.startFollow(logo);
     }
-    </script>
-
-</body>
-</html>
