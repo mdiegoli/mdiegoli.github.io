1692a488e8734fa2cf5869b68dcd8478dc4f13c0
Michele <m.diegoli@scsitaly.com>
2018-11-30 11:52:58 +0100

    Update new_engine.js

--- new_engine.js
+++ new_engine.js
@@ -1,13 +1,3 @@
-/*
-var context = document.getElementById('canvas1').getContext("2d");
-	
-var img = new Image();
-img.onload = function () {
-    context.drawImage(img, 0, 0);
-}
-img.src = "images/watermelon-duck.png";
-*/
-
 class BNEngine{
 	constructor(){
 		utils.getEBTN()
