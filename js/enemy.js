deac53b7c26a59f88b4c4c786f7983952a116fe6
Michele <morticcino@diegoli.org>
2018-12-06 12:47:22 +0100

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -1,8 +1,6 @@
-class enemy{
+class enemy extends entity{
     constructor(level){
-        this.level = level;
-        if(!this.__proto__.images)this.__proto__.images = {};
-        if(!this.__proto__.images[level])this.__proto__.images[level] = {};
+        super(level)
     }
 	animation(){
 		return new Promise((res,rej)=>{
@@ -23,32 +21,6 @@ class enemy{
 		})
 		
 	}
-    start(){
-        var me = this;
-        return new Promise((res,rej)=>{
-                me.preload()
-                .then(
-                    (succ)=>{
-                        me.create()
-                            .then(
-                                (succ)=>{
-                                    res('done');
-                                }
-                            )
-                            .catch(
-                                (err)=>{
-                                    res(err);
-                                }
-                            )
-                    }
-                )
-                .catch(
-                    (err)=>{
-                        console.log(err);
-                    }
-                )
-            })
-    }
     
     preload(){
          var me = this;
@@ -88,42 +60,7 @@ class enemy{
             )
         })
         
-    }
-
-    create(){
-        var me = this;
-        return new Promise((res,rej)=>{
-            if(me.dead === undefined){
-                me.dead = false;
-                me.randomX = 0;
-                me.randomY = Utils.random(1,gC.spritePosY);
-                
-            }else{
-                if(me.dead){
-                    alert('enemy dead')
-                }
-            }
-	    Utils.drawImages(me.__proto__.images[this.level], me.randomX, me.randomY);
-            
-            res();
-            
-        })
-        
-        
-    }
-	
-paint(x,y){
-        var me = this;
-        return new Promise((res,rej)=>{
-            Utils.drawImages(me.__proto__.images[this.level], x, y);
-            
-            res();
-            
-        })
-        
-        
-    }
-    
+    }    
 
 }
 
