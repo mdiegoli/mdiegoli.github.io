064b4296bc9622bc864df68ece7661bec76c6c96
Michele <morticcino@diegoli.org>
2018-12-07 15:01:32 +0100

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -5,22 +5,7 @@ class bullet extends entity{
 	
     create(){
         var me = this;
-        return new Promise((res,rej)=>{
-            if(me.dead === undefined){
-                me.dead = false;
-                me.randomX = gC.spritePosX/2;
-                me.randomY = gC.spritePosY;
-                
-            }else{
-                if(me.dead){
-                    alert('enemy dead')
-                }
-            }
-	    Utils.drawImages(me.__proto__.images[this.level], me.randomX, me.randomY);
-            
-            res();
-            
-        })
+         Utils.drawImages(me.__proto__.images[this.level], me.randomX, me.randomY);
         
         
     }
@@ -33,34 +18,12 @@ class bullet extends entity{
 			this.indexes = {}
 		
 	    if(!this.indexes.LW)
-            this.indexes.LW= Utils.random(1,36).toString().padStart(2,'0');
-        if(!this.indexes.RW)
-            this.indexes.RW= this.indexes.LW;
-        if(!this.indexes.LB)
-            this.indexes.LB= Utils.random(1,36).toString().padStart(2,'0');
-        if(!this.indexes.HE)
-            this.indexes.HE= Utils.random(1,36).toString().padStart(2,'0');
-        if(!this.indexes.BO)
-            this.indexes.BO= Utils.random(1,36).toString().padStart(2,'0');
+            this.indexes.BU= Utils.random(1,36).toString().padStart(2,'0');
+       
 		
-		var lI = Utils.loadImage;
-		preloaded.push(lI(me.__proto__.images[me.level],'assets/games/demons/'+gC.demonData['LW'][this.indexes.LW].img, 'LW'));
-		preloaded.push(lI(me.__proto__.images[me.level],'assets/games/demons/'+gC.demonData['RW'][this.indexes.RW].img, 'RW'));
-		preloaded.push(lI(me.__proto__.images[me.level],'assets/games/demons/'+gC.demonData['LB'][this.indexes.LB].img, 'LB'));
-		preloaded.push(lI(me.__proto__.images[me.level],'assets/games/demons/'+gC.demonData['BO'][this.indexes.BO].img, 'BO'));
-		preloaded.push(lI(me.__proto__.images[me.level],'assets/games/demons/'+gC.demonData['HE'][this.indexes.HE].img, 'HE'));
-
-		Promise.all(preloaded)
-            .then(
-                (succ)=>{
-                res();
-                }
-            )
-            .catch(
-                (err)=>{
-                rej();
-                }
-            )
+	var lI = Utils.loadImage;
+	lI(me.__proto__.images[me.level],'assets/games/bullets/hero1.png', 'LW');
+	res();
         })
         
     }    
