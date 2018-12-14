502dddeab98c2c5073ac076a4efb86a11c02e9d8
Michele <morticcino@diegoli.org>
2018-12-14 09:59:37 +0100

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -39,7 +39,7 @@ class bullet extends entity{
             	this.indexes.BU= Utils.random(1,36).toString().padStart(2,'0');
         if(!me.randomX && !me.randomY){
 			me.randomX = gC.player.getPosX();
-			me.randomY = gC.spritePosY-gC.spriteH;
+			me.randomY = gC.player.getPosY();
 		}
 		
 		var lI = Utils.loadImage;
