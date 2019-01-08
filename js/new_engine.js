6ba1da5da5e2a82af6f57ab51a307775b25c6470
Michele <morticcino@diegoli.org>
2019-01-08 17:13:03 +0100

    Update new_engine.js

--- js/new_engine.js
+++ js/new_engine.js
@@ -35,8 +35,8 @@ function startGame(){
 		)
         if(a==(a_l-1)){
         
-        //Utils.c2osc();
-	Utils.c2c();
+        Utils.c2osc();
+	//Utils.c2c();
         requestAnimationFrame(this.gAF)
         }
 	}
@@ -191,8 +191,8 @@ function addCanvas(){
             var a = Utils.getEBI('box');
             var b = Utils.createE('canvas');
             //memorizzo canvas e contesto
-            Utils.setCanvas(b)
-            //Utils.setOffScreen(b)
+            //Utils.setCanvas(b)
+            Utils.setOffScreen(b)
             Utils.appendB2A(a,b)
             Utils.setAttribute(b,'width',gC.width)
             Utils.setAttribute(b,'height',gC.height)
