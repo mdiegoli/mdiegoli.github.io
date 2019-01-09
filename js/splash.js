5da915fca1c025c0de02731a1a9c13033ef6ba9b
Michele <morticcino@diegoli.org>
2019-01-09 09:36:32 +0100

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -41,6 +41,28 @@ class splash{
         
         
     })
+		   
+	createOffscreen(){
+        var me = this;
+        return new Promise((res,rej)=>{
+        //Utils.clearCanvas('#000');
+        Utils.drawImage(me.images['SP'],100,100);
+        Utils.drawText('Press s to start',100,200,'#FFF') 
+        function k(e){
+            switch(e.keyCode){
+                case 83:
+                    document.removeEventListener('keydown',k,false)
+        
+                    res();
+                    break;
+                
+            }
+        }
+        document.addEventListener('keydown',k,false)
+        Utils.c2osc();
+        
+        
+    })
 }
 
 }
