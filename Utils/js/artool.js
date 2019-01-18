a07f01f6fa6e35154c9ff2ebab6032bed4d7e9db
Michele <morticcino@diegoli.org>
2019-01-18 15:18:59 +0100

    Update artool.js

--- Utils/js/artool.js
+++ Utils/js/artool.js
@@ -1,6 +1,28 @@
 var artool = class {
   contructor(){
-    
+    var me = this;
+    return new Promise(function(res,rej){
+        var canvas = Utils.getEBTN('canvas')
+        if(typeof canvas === 'object' && canvas.length <= 0){
+            //non c'è canvas
+            var a = Utils.getEBTN('body')[0]
+            var b = Utils.createE('canvas')
+            //memorizzo canvas e contesto
+            Utils.setCanvas(b)
+            Utils.appendB2A(a,b)
+	    gC.canvas = b;
+            Utils.setAttribute(b,'width',window.innerWidth)
+            Utils.setAttribute(b,'height',window.innerHeight)
+            Utils.setAttribute(b,'style','touch-action:none')
+		a.addEventListener('mousedown',onMouseDown)
+		a.addEventListener('mouseup',onMouseUp)
+		a.addEventListener('mousemove',onMouseMove)
+		a.addEventListener('touchstart',onTouchStart)
+		a.addEventListener('touchend',onTouchEnd)
+		a.addEventListener('touchmove',onTouchMove)
+        }
+        res();
+    })
   }
   
   onMouseDown(){}
