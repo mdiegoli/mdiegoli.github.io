96ee6ee9c7fe41f297199e634530023f3d9659eb
Michele <morticcino@diegoli.org>
2019-01-18 15:09:22 +0100

    Create artool.js

---
+++ Utils/js/artool.js
@@ -0,0 +1,15 @@
+var artool = class {
+  contructor(){
+    
+  }
+  
+  onMouseDown(){}
+  onMouseUp(){}
+  onMouseMove(){}
+  onTouchStart(){}
+  onTouchEnd(){}
+  onTouchMove(){}
+  
+  getMouseCoord(e){}
+  getTouchCoord(e){}
+}
