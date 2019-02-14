fc70e744838c7cc6c4920e3579ef3c6593246015
Michele <morticcino@diegoli.org>
2019-02-14 13:50:41 +0100

    Update toolMatSat.js

--- Utils/js/toolMatSat.js
+++ Utils/js/toolMatSat.js
@@ -1,67 +1,8 @@
 'use strict';
 
-class toolMatSat{
+class toolMatSat extend tool{
 	constructor(){
-		this.setEvents();
-	}
-	
-	setEvents(){
-		var b = Utils.getEBTN('canvas')[0]
-		b.addEventListener('mousedown',this.mouseDown.bind(this))
-		b.addEventListener('mouseup',this.mouseUp.bind(this))
-		b.addEventListener('mousemove',this.mouseMove.bind(this))
-		b.addEventListener('touchstart',this.touchDown.bind(this))
-		b.addEventListener('touchend',this.touchUp.bind(this))
-		b.addEventListener('touchmove',this.touchMove.bind(this))
-	}
-
-
-	mouseMove(e){
-		e.preventDefault();
-		if(gC.paint){
-			let coord = Utils.getRealMousePos(gC.canvas,e)
-			this.custom_mouseMove(coord)
-		}
-	}
-
-	mouseDown(e){
-	    e.preventDefault()
-	    let coord = Utils.getRealMousePos(gC.canvas,e)
-	    gC.oldCoordX = coord.x
-	    gC.oldCoordY = coord.y
-		gC.paint = true;
-		this.custom_mouseDown(coord);
-	}
-
-	mouseUp(e){
-		e.preventDefault();
-	    gC.paint = false;
-	    let coord = Utils.getRealMousePos(gC.canvas,e)
-		this.custom_mouseUp(coord)
-	}
-
-	touchMove(e){
-	    e.preventDefault()
-		if(gC.paint){
-			let coord = Utils.getRealTouchPos(gC.canvas,e)
-			this.custom_touchMove(coord);
-		}
-	}
-
-	touchDown(e){
-	    e.preventDefault()
-	    let coord = Utils.getRealTouchPos(gC.canvas,e)
-	    gC.oldCoordX = coord.x
-	    gC.oldCoordY = coord.y
-		gC.paint = true;
-		this.custom_touchDown(coord);
-	}
-
-	touchUp(e){
-	    e.preventDefault()
-		//let coord = Utils.getRealTouchPos(gC.canvas,e)
-	    gC.paint = false;
-		this.custom_touchUp();
+		super()
 	}
 
 	custom_mouseMove(coord){
