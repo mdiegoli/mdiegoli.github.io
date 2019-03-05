85832a5148fd29b998666cf2f9309fd7f0efb532
Michele <morticcino@diegoli.org>
2019-03-05 21:40:56 +0100

    get random seed from the max number of demon pieces kind

--- js/enemy.js
+++ js/enemy.js
@@ -18,15 +18,15 @@ class enemy extends entity{
 			this.indexes = {}
 		
 	    if(!this.indexes.LW)
-            this.indexes.LW= Utils.random(1,36).toString().padStart(2,'0');
+            this.indexes.LW= Utils.random( 1, Object.keys(gC.demonData.LW).length ).toString().padStart(2,'0');
         if(!this.indexes.RW)
             this.indexes.RW= this.indexes.LW;
         if(!this.indexes.LB)
-            this.indexes.LB= Utils.random(1,36).toString().padStart(2,'0');
+            this.indexes.LB= Utils.random(1, Object.keys(gC.demonData.LB).length ).toString().padStart(2,'0');
         if(!this.indexes.HE)
-            this.indexes.HE= Utils.random(1,36).toString().padStart(2,'0');
+            this.indexes.HE= Utils.random(1, Object.keys(gC.demonData.HE).length ).toString().padStart(2,'0');
         if(!this.indexes.BO)
-            this.indexes.BO= Utils.random(1,36).toString().padStart(2,'0');
+            this.indexes.BO= Utils.random(1, Object.keys(gC.demonData.BO).length ).toString().padStart(2,'0');
 		
 		var lI = Utils.loadImage;
 		preloaded.push(lI(me.__proto__.images[me.level],'assets/games/demons/'+gC.demonData['LW'][this.indexes.LW].img, 'LW'));
