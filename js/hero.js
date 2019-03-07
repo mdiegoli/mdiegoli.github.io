5b6c1643fe995fea0d51569dbfbbad5f36c4566a
Michele <morticcino@diegoli.org>
2019-03-07 08:20:24 +0100

    Update hero.js

--- js/hero.js
+++ js/hero.js
@@ -53,15 +53,15 @@ class hero extends entity{
 			this.indexes = {}
 		
 	    if(!this.indexes.LW)
-            this.indexes.LW= Utils.random(1,5).toString().padStart(2,'0');
+            this.indexes.LW= Utils.random(1,Object.keys(gC.shipData.LW).length).toString().padStart(2,'0');
         if(!this.indexes.RW)
             this.indexes.RW= this.indexes.LW;
         if(!this.indexes.LB)
-            this.indexes.LB= Utils.random(1,5).toString().padStart(2,'0');
+            this.indexes.LB= Utils.random(1,Object.keys(gC.shipData.LB).length).toString().padStart(2,'0');
         if(!this.indexes.HE)
-            this.indexes.HE= Utils.random(1,5).toString().padStart(2,'0');
+            this.indexes.HE= Utils.random(1,Object.keys(gC.shipData.HE).length).toString().padStart(2,'0');
         if(!this.indexes.BO)
-            this.indexes.BO= Utils.random(1,5).toString().padStart(2,'0');
+            this.indexes.BO= Utils.random(1,Object.keys(gC.shipData.BO).length).toString().padStart(2,'0');
 		
 		var lI = Utils.loadImage;
 		preloaded.push(lI(me.__proto__.images[me.level],'assets/games/demonship/demonship/'+gC.shipData['LW'][this.indexes.LW].img, 'LW'));
