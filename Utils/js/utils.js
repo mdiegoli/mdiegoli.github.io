a658ba55f0b7e0a77c5aa4b9c2f8a9e915c7b93e
Michele <morticcino@diegoli.org>
2019-03-14 08:54:12 +0100

    Update utils.js

--- Utils/js/utils.js
+++ Utils/js/utils.js
@@ -92,6 +92,7 @@ setCanvas3D(e){
             gC.texture = new THREE.Texture(me.co); //new THREE.TextureLoader( );
             gC.texture.needsUpdate = true;
 	gC.texture.repeat.set(2,2)
+			gC.texture.offset.set(0.5,0.5)
 
              //objects.push( plane );
 			gC.plane = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({ map : gC.texture }) );
