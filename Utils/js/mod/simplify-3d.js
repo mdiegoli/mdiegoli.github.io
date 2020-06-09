06f2418306692c6bb9c9f6c768627be028b5e401
Michele <morticcino@diegoli.org>
2020-06-09 18:30:26 +0200

    Update simplify-3d.js

--- Utils/js/mod/simplify-3d.js
+++ Utils/js/mod/simplify-3d.js
@@ -67,4 +67,4 @@ function getSquareSegmentDistance(p, p1, p2) { // square distance from a point t
          dy * dy;
 }
 
-export simplify(getSquareDistance, getSquareSegmentDistance); 
+export { simplify(getSquareDistance, getSquareSegmentDistance) }
